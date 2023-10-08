55cc540f704cbef99496957023d84104b2911076
techmoves <muhndlovu28@gmail.com>
2023-10-08 22:11:05 +0200

    Add api end point

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+  get 'greetings/random', to: 'greetings#random'
+end
+
 end
